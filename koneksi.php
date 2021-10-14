e9dd1561df4a23b283135fb481b0123f94cb11d5
unknown <vaniamelia202002@gmail.com>
2021-10-14 07:44:11 -0700

    upload koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,14 @@
-?php>
+<?php
+$server="localhost";
+$user="root";
+$database="phpregpagi";
+$password="";
+
+$koneksi=mysqli_connect($server,$user,$password,$database);
+
+if($koneksi){
+    echo "koneksi sukses";
+}else{
+    echo "koneksi gagal";
+}
+ ?>
