f2de06b93c6a1b4ee7db45eb724a34ffed65a11e
unknown <vaniamelia202002@gmail.com>
2021-10-14 18:22:34 -0700

    saya mau upload view_anggota

---
+++ view_anggota.php
@@ -0,0 +1,24 @@
+<?php
+include ("koneksi.php");
+$query_view=mysqli_query($koneksi,"select * from anggota");
+
+?>
+<table class="table table-bordered" border="1">
+  <tr>
+    <td>No</td>
+    <td>Nama</td>
+    <td>Alamat</td>
+    <td>No Tlpn</td>
+  </tr>
+  <?php
+  $no=1;
+  while($tampil=mysqli_fetch_array($query_view)){?>
+    <tr>
+      <td><?php echo $no++;?></td>
+      <td><?php echo $tampil ['nama'];?></td>
+      <td><?php echo $tampil ['alamat'];?></td>
+      <td><?php echo $tampil ['no-tlpn'];?></td>
+  </tr>
+<?php }?>
+
+</table>
