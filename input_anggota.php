36c5d2f73eb8c037c31c474f4b85dc448c75c344
unknown <vaniamelia202002@gmail.com>
2021-10-29 05:52:09 -0700

    mengupload input_anggota

--- input_anggota.php
+++ input_anggota.php
@@ -17,22 +17,20 @@ if($query) {
 ?>
 <form method="POST">
 <table class="table table-bordered" border="1">
-    <tr>
-         <td>Nama</td>
+  <tr>
+    <td>Nama</td>
 		<td><input type="text" name="nama" class="form-control"></td>
 	</tr>	
-	    <tr>
-         <td>Alamat</td>
+	<tr>
+    <td>Alamat</td>
 		<td><input type="text" name="alamat" class="form-control"></td>
 	</tr>	
-	    <tr>
-         <td>No Telepon</td>
+	<tr>
+    <td>No Telepon</td>
 		<td><input type="text" name="no_tlpn" class="form-control"></td>
 	</tr>	
-	
-	</tr>
-		<tr>
-			<td><input type="submit" name="save" class="btn btn-danger"></td>
+	<tr>
+		<td><input type="submit" name="save" class="btn btn-danger"></td>
 		</tr>
 </table>
 </form>
