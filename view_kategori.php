ca322ae81efb517c814d107d3380cfc1fc5656b3
unknown <vaniamelia202002@gmail.com>
2021-11-07 00:33:20 -0700

    terbaru view_barang

--- view_kategori.php
+++ view_kategori.php
@@ -3,10 +3,12 @@ include ("koneksi.php");
 $query_view=mysqli_query($koneksi,"select * from kategori");
 
 ?>
+<a href="input_kategori.php" class="btn btn-danger">Tambah kategori</a>
 <table class="table table-bordered" border="1">
   <tr>
     <td>NO</td>
     <td>Nama</td>
+    <td colspan="4">Action</td>
   </tr>
   <?php
   $no=1;
@@ -14,7 +16,8 @@ $query_view=mysqli_query($koneksi,"select * from kategori");
     <tr>
       <td><?php echo $no++;?></td>
       <td><?php echo $tampil ['nama'];?></td>
-     
+      <td><a href="edit_kategori.php?id_kategori=<?php echo $tampil ['id_kategori'];?>">Edit</a></td>
+      <td><a href="hapus_kategori.php?id_kategori=<?php echo $tampil ['id_kategori'];?>">hapus</a></td>
   </tr>
 <?php }?>
 
