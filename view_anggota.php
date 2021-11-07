24a15f17bf42bb81078948d17335cf0ac5ea6835
unknown <vaniamelia202002@gmail.com>
2021-11-07 00:27:25 -0700

    terbaru view_anggota

--- view_anggota.php
+++ view_anggota.php
@@ -3,12 +3,15 @@ include ("koneksi.php");
 $query_view=mysqli_query($koneksi,"select * from anggota");
 
 ?>
+</br>
+<a href="input_anggota.php" class="btn btn-danger">Tambah anggota</a>
 <table class="table table-bordered" border="1">
   <tr>
     <td>No</td>
     <td>Nama</td>
     <td>Alamat</td>
     <td>No Tlpn</td>
+    <td colspan="4">Action</td>
   </tr>
   <?php
   $no=1;
@@ -18,6 +21,8 @@ $query_view=mysqli_query($koneksi,"select * from anggota");
       <td><?php echo $tampil ['nama'];?></td>
       <td><?php echo $tampil ['alamat'];?></td>
       <td><?php echo $tampil ['no_tlpn'];?></td>
+      <td><a href="edit_anggota.php?id_anggota=<?php echo $tampil ['id_anggota'];?>">Edit</a></td>
+      <td><a href="hapus_anggota.php?id_anggota=<?php echo $tampil ['id_anggota'];?>">hapus</a></td>
   </tr>
 <?php }?>
 
