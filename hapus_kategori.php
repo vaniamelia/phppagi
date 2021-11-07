8bada9e8d14bf0ae340cb4091475f6d4fc6b1dd2
unknown <vaniamelia202002@gmail.com>
2021-11-07 07:07:58 -0800

    buang hapus_kategori

--- hapus_kategori.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("koneksi.php");
-$query_delete=mysqli_query($koneksi,"delete from kategori where id_kategori='".$_GET['id_kategori']."'");
-if($query_delete){
-  header("location:view_kategori.php");
-}else{
-  echo mysqli_error();
-}
