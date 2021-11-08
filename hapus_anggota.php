84fd478b4257b92a9f0fd817f5dab6bedb4f4476
vaniamelia <91582425+vaniamelia@users.noreply.github.com>
2021-11-07 17:38:17 -0800

    buang hapus_anggota.php

--- hapus_anggota.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("koneksi.php");
-$query_delete=mysqli_query($koneksi,"delete from anggota where id_anggota='".$_GET['id_anggota']."'");
-if($query_delete){
-  header("location:view_anggota.php");
-}else{
-  echo mysqli_error();
-}
