64b28154286d7887169952c6552e07ecce9f4d48
vaniamelia <91582425+vaniamelia@users.noreply.github.com>
2021-11-15 15:05:16 +0700

    tampilan nih

---
+++ home.php
@@ -0,0 +1,35 @@
+<?php
+
+//mengaktifkan session
+session_start();
+
+//cek apakah user telah login,jika belum login maka di alihkan kehalaman login
+if($_SESSION['level']== ""){
+    echo "<SCRIPT>alert(silahkan login terlebih dahulu !');window.location='index.php'<SCRIPT>";
+} 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+<div class="kolom">
+    <div>
+        <?php echo " Hai,Selamat Datang <b>".$_SESSION['username']."</b>";
+        ?>
+    <ul>
+        <li><a href="view_anggota.php">Daftar anggota</a></li>
+        <li><a href="view_barang.php">Daftar barang</a></li>
+        <li><a href="view_kategori.php">Daftar kategori</a></li>
+    </ul>
+
+    <br/>
+    <a href="logout.php" onclick="return confirm('yakin keluar?')">keluar</a>
+    </div>  
+</body>
+</html>
