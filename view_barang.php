6e8ec2680742f8fdf8f41eace851bc1b17439664
vaniamelia <91582425+vaniamelia@users.noreply.github.com>
2021-11-15 14:45:30 +0700

    ini juga diubah

--- view_barang.php
+++ view_barang.php
@@ -1,16 +1,23 @@
 <?php
 include ("koneksi.php");
+session_start();
+if(!($_SESSION['username'])){
+  echo "<SCRIPT>alert('silahkan login terlebih dahulu !');window.location='index.php'</SCRIPT>";
+}
 $query_view=mysqli_query($koneksi,"select * from barang");
-
 ?>
 </br>
-<a href="input_barang.php" class="btn btn-danger">Tambah barang</a>
+<?php
+if($_SESSION['level']=="admin"){
+  echo '<a href="input_barang.php" class="btn btn-danger">Tambah barang</a>';
+}?> <a href="home.php">Halaman Utama</a>
 <table class="table table-bordered" border="1">
   <tr>
-    <td>NO</td>
-    <td>ID KATEGORI</td>
-    <td>NAMA</td>
+    <td>No</td>
+    <td>id_kategori</td>
+    <td>Nama</td>
     <td colspan="4">Action</td>
+    <?php if($_SESSION['level']=="admin")?>
   </tr>
   <?php
   $no=1;
@@ -19,8 +26,8 @@ $query_view=mysqli_query($koneksi,"select * from barang");
       <td><?php echo $no++;?></td>
       <td><?php echo $tampil ['id_kategori'];?></td>
       <td><?php echo $tampil ['nama'];?></td>
-      <td><a href="edit_barang.php?id_barang=<?php echo $tampil ['id_barang'];?>">Edit</a></td>
-      <td><a href="hapus_barang.php?id_barang=<?php echo $tampil ['id_barang'];?>">hapus</a></td>
+      <?php if($_SESSION['level']=="admin"){ echo '<td><a href="edit_barang.php?id_barang='.$tampil ['id_barang'].'">Edit</a></td>';
+      echo '<td><a href="hapus_barang.php?id_barang='.$tampil ['id_barang'].'" onclick="return confirm("yakin keluar?")">hapus</a></td>';}?>
   </tr>
 <?php }?>
 
