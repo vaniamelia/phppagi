8977220b35d1e3e382cea3b6f73d356f9ccc4426
vaniamelia <91582425+vaniamelia@users.noreply.github.com>
2021-11-15 17:46:19 +0700

    baru aja

--- view_kategori.php
+++ view_kategori.php
@@ -4,13 +4,13 @@ session_start();
 if(!($_SESSION['username'])){
   echo "<SCRIPT>alert('silahkan login terlebih dahulu !');window.location='index.php'</SCRIPT>";
 }
-$query_view=mysqli_query($koneksi,"select * from barang");
+$query_view=mysqli_query($koneksi,"select * from kategori");
 
 ?>
 </br>
 <?php
 if($_SESSION['level']=="admin"){
-  echo '<a href="input_barang.php" class="btn btn-danger">Tambah barang</a>';
+  echo '<a href="input_kategori.php" class="btn btn-danger">Tambah Kategori</a>';
 }?> <a href="home.php">Halaman Utama</a>
 <table class="table table-bordered" border="1">
   <tr>
@@ -27,8 +27,8 @@ if($_SESSION['level']=="admin"){
       <td><?php echo $no++;?></td>
       <td><?php echo $tampil ['id_kategori'];?></td>
       <td><?php echo $tampil ['nama'];?></td>
-      <?php if($_SESSION['level']=="admin"){ echo '<td><a href="edit_barang.php?id_barang='.$tampil ['id_barang'].'">Edit</a></td>';
-      echo '<td><a href="hapus_barang.php?id_barang='.$tampil ['id_barang'].'" onclick="return confirm("yakin keluar?")">hapus</a></td>';}?>
+      <?php if($_SESSION['level']=="admin"){ echo '<td><a href="edit_kategori.php?id_kategori='.$tampil ['id_kategori'].'">Edit</a></td>';
+      echo '<td><a href="hapus_kategori.php?id_kategori='.$tampil ['id_kategori'].'" onclick="return confirm("yakin keluar?")">hapus</a></td>';}?>
   </tr>
 <?php }?>
 
