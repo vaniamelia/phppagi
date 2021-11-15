6971d669895130ccf10f4f9426b5fdb91bfab0d8
vaniamelia <91582425+vaniamelia@users.noreply.github.com>
2021-11-15 20:13:57 +0700

    punya saya

--- home.php
+++ home.php
@@ -1,35 +1,35 @@
-<?php
-
-//mengaktifkan session
+<?php 
+include 'koneksi.php';
+ 
+// mengaktifkan session
 session_start();
-
-//cek apakah user telah login,jika belum login maka di alihkan kehalaman login
-if($_SESSION['level']== ""){
-    echo "<SCRIPT>alert(silahkan login terlebih dahulu !');window.location='index.php'<SCRIPT>";
-} 
+ 
+// cek apakah user telah login, jika belum login maka di alihkan ke halaman login
+if(!($_SESSION['level'] == "")){
+	echo "<SCRIPT>alert('silahkan login terlebih dahulu');window.location='index.php'</SCRIPT>";
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home</title>
 </head>
 <body>
 <div class="kolom">
-    <div>
-        <?php echo " Hai,Selamat Datang <b>".$_SESSION['username']."</b>";
-        ?>
-    <ul>
-        <li><a href="view_anggota.php">Daftar anggota</a></li>
-        <li><a href="view_barang.php">Daftar barang</a></li>
-        <li><a href="view_kategori.php">Daftar kategori</a></li>
-    </ul>
-
-    <br/>
-    <a href="logout.php" onclick="return confirm('yakin keluar?')">keluar</a>
-    </div>  
+	<div>
+		<?php echo "Hai, Selamat Datang <b>".$_SESSION['username']."</b>";
+ 		?>
+	<ul>
+		<li><a href="view_anggota.php">Daftar Anggota</a></li>
+		<li><a href="view_barang.php">Daftar Barang</a></li>
+		<li><a href="view_kategori.php">Daftar Kategori</a></li>
+ 	</ul>
+	 
+	<br/>
+	<a href="logout.php" onclick="return confirm('Yakin keluar?')">Keluar</a>	
+</div>
 </body>
 </html>
