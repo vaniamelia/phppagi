b5b609a789c68e0bd8228ac28abf200b5944d34f
vaniamelia <91582425+vaniamelia@users.noreply.github.com>
2021-11-15 20:15:14 +0700

    kirim nih

--- cek_login.php
+++ cek_login.php
@@ -9,16 +9,17 @@ $cek = mysqli_num_rows($login);
 
 // cek apakah username dan password di temukan pada database
 if($cek > 0){
- $data = mysqli_fetch_assoc($login);
+
+ 	$data = mysqli_fetch_assoc($login);
 	if($data['status']=="1"){
-		
 		// cek jika user login sebagai admin
 		if($data['level']=="admin"){
-		// buat session login dan username
-		$_SESSION['username'] = $username;
-		$_SESSION['level'] = "admin";
-		// alihkan ke halaman dashboard admin
-		header("location:home.php");
+
+			// buat session login dan username
+			$_SESSION['username'] = $username;
+			$_SESSION['level'] = "admin";
+			// alihkan ke halaman dashboard admin
+			header("location:home.php");
  
 		// cek jika user login sebagai host
 		}else if($data['level']=="host"){
